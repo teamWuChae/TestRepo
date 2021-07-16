fdf8e348f480a9794699b83975124e89a245ada0
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2021-07-16 17:38:21 +0900

    Create optionActivity.java

---
+++ optionActivity.java
@@ -0,0 +1,14 @@
+package com.example.rhythmproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class optionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option);
+    }
+}
