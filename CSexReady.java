baccfa367e95d991884841fc91016dd82a5b4e3d
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2021-07-17 13:11:34 +0900

    Create CSexReady.java

---
+++ CSexReady.java
@@ -0,0 +1,14 @@
+package com.example.rhythmproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CSexReady extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_csex_ready);
+    }
+}
