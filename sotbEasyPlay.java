4050d8f062562ed7198b55721f97bf8c8a0d741d
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2021-07-17 13:13:18 +0900

    Create sotbEasyPlay.java

---
+++ sotbEasyPlay.java
@@ -0,0 +1,14 @@
+package com.example.rhythmproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sotbEasyPlay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sotb_easy_play);
+    }
+}
